8b70638f099fcc8f0a9dab629a62bd30a508136b
RroniMoni <85192845+rroni69@users.noreply.github.com>
2026-04-22 14:01:03 +0200

    some small changes to improve functionality

--- server/src/config/index.js
+++ server/src/config/index.js
@@ -2,7 +2,7 @@ import dotenv from 'dotenv'
 dotenv.config()
 
 const config = {
-  port: process.env.PORT || 5000,
+  port: process.env.PORT || 5004,
   nodeEnv: process.env.NODE_ENV || 'development',
   mongodbUri: process.env.MONGODB_URI || 'mongodb://localhost:27017/skill-path-architect',
   jwt: {
